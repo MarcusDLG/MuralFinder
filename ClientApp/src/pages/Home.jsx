a991f9dc5688cd2f69cc1745c9ae9ed427909cdf
MarcusDLG <delag010@gmail.com>
2020-04-06 17:06:44 -0400

    splash page complete, search page needs mapbox

--- ClientApp/src/pages/Home.jsx
+++ ClientApp/src/pages/Home.jsx
@@ -3,6 +3,9 @@ import Hero from '../components/Hero'
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSprayCan } from '@fortawesome/free-solid-svg-icons'
+import { faMapMarkedAlt } from '@fortawesome/free-solid-svg-icons'
+import { faCameraRetro } from '@fortawesome/free-solid-svg-icons'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
 
 export function Home() {
   return (
@@ -11,23 +14,43 @@ export function Home() {
         <section className="hero">
           <Hero />
         </section>
+        <section className="who-we-are">
+          <FontAwesomeIcon icon={faCameraRetro} className="retro-camera" />{' '}
+          <h1>Mural Book:</h1>
+          <p>
+            Stumbling across the murals of Tampa Bay can be an extremely fun
+            process, but what happens when you don't know where to find them or
+            which ones you've seen? We've done the searching, now go get that
+            picture!
+          </p>
+        </section>
         <section className="cta">
-          <h3 className="hero-header">
+          {/* <h3 className="hero-header">
             Helping you find the murals of Tampa Bay!
-          </h3>
+          </h3> */}
           <section className="description-of-us">
-            <p className="description">
-              After a Sunday out and about our great community, we realized that
-              our cities have great murals, but no way to find them . . . until
-              now!
-            </p>
-          </section>
-          <Link to="/find" className="call-to-action">
             <FontAwesomeIcon icon={faSprayCan} className="spray-can" />
-            Find Murals
-          </Link>
+            <p className="description">
+              Come see the treasured murals of Tampa Bay!
+            </p>
+            <Link to="/find" className="call-to-action">
+              <FontAwesomeIcon icon={faMapMarkedAlt} className="map" />
+              Find Murals
+            </Link>
+          </section>
         </section>
       </main>
+      <footer>
+        <p>
+          Made with{' '}
+          <span>
+            <FontAwesomeIcon icon={faHeart} className="footer-heart" />{' '}
+            <FontAwesomeIcon icon={faHeart} className="footer-heart" />{' '}
+            <FontAwesomeIcon icon={faHeart} className="footer-heart" />{' '}
+          </span>
+          by Kastle
+        </p>
+      </footer>
     </>
   )
 }
