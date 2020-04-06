49930005d98855a222b42fc494e4d5f34fdb69af
MarcusDLG <delag010@gmail.com>
2020-04-06 14:29:26 -0400

    styled landing page

--- ClientApp/src/components/Hero.jsx
+++ ClientApp/src/components/Hero.jsx
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom'
 const Hero = () => {
   return (
     <>
-      {/* <h1>Welcome to Mural Findah!</h1>
-      <h2>We're here to help you find the murals of Tampa Bay!</h2> */}
-      <Link to="/find" className="call-to-action">
+      {/* <Link to="/find" className="call-to-action">
         Find Murals
       </Link>
+      <h3>Welcome to Mural Findah!</h3>
+      <h2>We're here to help you find the murals of Tampa Bay!</h2> */}
     </>
   )
 }
--- ClientApp/src/pages/Home.jsx
+++ ClientApp/src/pages/Home.jsx
@@ -1,17 +1,33 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSprayCan } from '@fortawesome/free-solid-svg-icons'
 
 export function Home() {
   return (
     <>
-      <main>
+      <main className="home-main">
         <section className="hero">
           <Hero />
         </section>
+        <section className="cta">
+          <h3 className="hero-header">
+            Helping you find the murals of Tampa Bay!
+          </h3>
+          <section className="description-of-us">
+            <p className="description">
+              After a Sunday out and about our great community, we realized that
+              our cities have great murals, but no way to find them . . . until
+              now!
+            </p>
+          </section>
+          <Link to="/find" className="call-to-action">
+            <FontAwesomeIcon icon={faSprayCan} className="spray-can" />
+            Find Murals
+          </Link>
+        </section>
       </main>
-      <section className="who-we-are">
-        {/* icon and button to take them to next page */}
-      </section>
     </>
   )
 }
