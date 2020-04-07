e4d836e68707e0ad363571c3eafdffa3da8b76da
MarcusDLG <delag010@gmail.com>
2020-04-07 15:10:01 -0400

    mural details page setup and ready for html & css

---
+++ ClientApp/src/components/SingleMural.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const SingleMural = props => {
+  const { mural } = props
+  return <div>Looking at details for {mural.name}</div>
+}
+
+export default SingleMural
--- ClientApp/src/pages/MuralDetails.jsx
+++ ClientApp/src/pages/MuralDetails.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import PageLoader from '../components/PageLoader'
+import SingleMural from '../components/SingleMural'
 
 const MuralDetails = props => {
   console.log(props)
-  return <div>Looking at details for a trail</div>
+  const muralId = props.match.params.muralId
+
+  const [mural, setMural] = useState({})
+
+  const getMuralData = async () => {
+    const resp = await axios.get('/api/murals/' + muralId)
+    console.log(resp.data)
+    setMural(resp.data)
+  }
+
+  useEffect(() => {
+    getMuralData()
+  }, [])
+  if (mural) {
+    return <SingleMural mural={mural} />
+  } else {
+    return <PageLoader />
+  }
 }
 
 export default MuralDetails
