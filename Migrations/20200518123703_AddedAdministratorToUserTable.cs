9d2e675f6144658e987fa53717ac4c745bb266a6
MarcusDLG <delag010@gmail.com>
2020-05-18 07:50:37 -0500

    isAdmin added

---
+++ Migrations/20200518123703_AddedAdministratorToUserTable.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MuralFinder.Migrations
+{
+    public partial class AddedAdministratorToUserTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "isAdministrator",
+                table: "Users",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "isAdministrator",
+                table: "Users");
+        }
+    }
+}
