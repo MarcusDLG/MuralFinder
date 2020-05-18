a729210465accf5bce0db7a78d0109c766054b20
MarcusDLG <delag010@gmail.com>
2020-05-18 05:41:56 -0500

    administrator access setup

--- Models/User.cs
+++ Models/User.cs
@@ -8,6 +8,7 @@ namespace MuralFinder.Models
     public int Id { get; set; }
     public string Email { get; set; }
     public string FullName { get; set; }
+    // public bool isAdministrator { get; set; } = false;
     [JsonIgnore]
     public string HashedPassword { get; set; }
     public List<Bookmark> Bookmarks { get; set; } = new List<Bookmark>();
