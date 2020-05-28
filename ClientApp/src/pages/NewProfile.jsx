3987cb6270e8b21e5bb89be809b91c56cba8792a
MarcusDLG <delag010@gmail.com>
2020-05-28 12:42:16 -0400

    reactstrap card in new profile

--- ClientApp/src/pages/NewProfile.jsx
+++ ClientApp/src/pages/NewProfile.jsx
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import ReactMapGL, { Marker, GeolocateControl, Popup } from 'react-map-gl'
-// import {
-//   Card,
-//   CardImg,
-//   CardText,
-//   CardBody,
-//   CardTitle,
-//   CardSubtitle,
-//   Button,
-// } from 'reactstrap'
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button,
+} from 'reactstrap'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 import Footer from '../components/Footer'
@@ -50,58 +50,82 @@ const NewProfile = () => {
   }, [])
   return (
     <>
-      <section className="map2">
-        <section className="map-container2">
-          <ReactMapGL
-            {...viewport}
-            width="80vw"
-            mapboxApiAccessToken={process.env.REACT_APP_MAPBOXTOKEN}
-            onViewportChange={setViewport}
-          >
-            {showPopup && (
-              <Popup
-                latitude={parseFloat(selectedPlace.latitude)}
-                longitude={parseFloat(selectedPlace.longitude)}
-                closeButton={true}
-                closeOnClick={false}
-                onClose={() => setShowPopup(false)}
-                offsetTop={-5}
-              >
-                <div className="popup-window">
-                  <Link to={`/mural/${selectedPlace.id}`}>
-                    <img
-                      src={selectedPlace.imageUrl}
-                      alt={selectedPlace.name}
-                    />
-                  </Link>
-                </div>
-              </Popup>
-            )}
-            {profile.bookmarks.map(m => {
-              return (
-                <Marker
-                  latitude={parseFloat(m.mural.latitude)}
-                  longitude={parseFloat(m.mural.longitude)}
-                  offsetTop={-10}
+      <main>
+        <section className="map2">
+          <section className="map-container2">
+            <ReactMapGL
+              {...viewport}
+              width="90vw"
+              mapboxApiAccessToken={process.env.REACT_APP_MAPBOXTOKEN}
+              onViewportChange={setViewport}
+            >
+              {showPopup && (
+                <Popup
+                  latitude={parseFloat(selectedPlace.latitude)}
+                  longitude={parseFloat(selectedPlace.longitude)}
+                  closeButton={true}
+                  closeOnClick={false}
+                  onClose={() => setShowPopup(false)}
+                  offsetTop={-5}
                 >
-                  <section
-                    className="marker"
-                    onClick={() => markerClicked(m.mural)}
+                  <div className="popup-window">
+                    <Link to={`/mural/${selectedPlace.id}`}>
+                      <img
+                        src={selectedPlace.imageUrl}
+                        alt={selectedPlace.name}
+                      />
+                    </Link>
+                  </div>
+                </Popup>
+              )}
+              {profile.bookmarks.map(m => {
+                return (
+                  <Marker
+                    latitude={parseFloat(m.mural.latitude)}
+                    longitude={parseFloat(m.mural.longitude)}
+                    offsetTop={-10}
                   >
-                    <span role="img" aria-label="marker">
-                      📍
-                    </span>
-                  </section>
-                </Marker>
-              )
-            })}
-            <GeolocateControl
-              positionOptions={{ enableHighAccuracy: true }}
-              trackUserLocation={true}
-            />
-          </ReactMapGL>
+                    <section
+                      className="marker"
+                      onClick={() => markerClicked(m.mural)}
+                    >
+                      <span role="img" aria-label="marker">
+                        📍
+                      </span>
+                    </section>
+                  </Marker>
+                )
+              })}
+              <GeolocateControl
+                positionOptions={{ enableHighAccuracy: true }}
+                trackUserLocation={true}
+              />
+            </ReactMapGL>
+          </section>
         </section>
-      </section>
+        <ul>
+          {profile.bookmarks.map(m => {
+            return (
+              <li key={m.mural.id}>
+                {/* <img src={m.mural.imageUrl} alt="" />
+                <section className="mural-info">
+                  <p>{m.mural.name}</p>
+                </section> */}
+                <Card inverse className="mural-card">
+                  {/* <CardTitle>{m.mural.name}</CardTitle> */}
+                  <CardBody className="body">
+                    {/* <CardText>{m.mural.description}</CardText> */}
+                    <Button>
+                      <Link to={`./mural/${m.mural.id}`}>{m.mural.name}</Link>
+                    </Button>
+                  </CardBody>
+                  <CardImg src={m.mural.imageUrl} alt={m.mural.name} />
+                </Card>
+              </li>
+            )
+          })}
+        </ul>
+      </main>
     </>
   )
 }
