ed7d026c9d4f1526420029c371253335bfc5351a
Marcus De La Garza <delag010@gmail.com>
2021-08-15 09:20:07 -0500

    Command listed in deploy file

--- deploy.sh
+++ deploy.sh
@@ -6,3 +6,5 @@ docker tag mural-finder-image registry.heroku.com/kastle-mural-finder/web
 docker push registry.heroku.com/kastle-mural-finder/web
 
 heroku container:release web -a kastle-mural-finder
+
+# ./deploy.sh to deploy updated image to heroku.
